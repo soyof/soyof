2b41d14389d70e0233b6e9f11d9828c1143265d5
Mr.H <44597139+osorso@users.noreply.github.com>
2023-07-17 22:56:27 +0800

    增加valine评论功能

---
+++ docs/.vuepress/components/ValineComment.vue
@@ -0,0 +1,46 @@
+<template>
+  <div id="valineComment"></div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      Valine: null
+    }
+  },
+  mounted() {
+    import('valine').then(res => {
+      this.Valine = res.default
+      this.handleInitValine()
+    })
+  },
+  methods: {
+    handleInitValine() {
+      new this.Valine({
+        el: '#valineComment',
+        appId: 'ErrD2Ef6tawtDoG7CYDLGZT4-MdYXbMMI',
+        appKey: 'yDTo3JsGFGAWKgN3nslMRSXs',
+        pageSize: 10,
+        placeholder: '有什么建议请告诉我...',
+        meta: ['nick', 'mail', 'link'],
+        lang: 'zh-CN',
+        visitor: true,
+        highlight: true,
+        avatarForce: false,
+        recordIP: true,
+        serverURLs: 'https://errd2ef6.api.lncldglobal.com',
+        requiredFields: [],
+        enableQQ: true,
+        avatar: 'monsterid',
+        emojiCDN: 'https://img.t.sinajs.cn/t4/appstyle/expression/ext/normal/',
+        emojiMaps: null
+      })
+    }
+  }
+}
+</script>
+
+<style scoped lang="stylus">
+
+</style>
