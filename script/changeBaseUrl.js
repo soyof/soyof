e44fc5f4e40b4bdb87f3ec7560969d505164c3a1
Mr.H <44597139+osorso@users.noreply.github.com>
2023-07-20 21:32:49 +0800

    更新

--- script/changeBaseUrl.js
+++ script/changeBaseUrl.js
@@ -8,11 +8,10 @@ const handleBaseUrl = (direct) => {
   if (fs.statSync(direct).isFile()) {
     if (path.extname(direct) === '.html' || path.extname(direct) === '.js' || path.extname(direct) === '.css') {
       const fileContent = fs.readFileSync(direct, 'utf-8')
-      // 替换文件中的文本
-      const updatedContent = fileContent.replace(/\/soyof\//g, '/')
-      // 将替换后的内容写回文件
-      fs.writeFileSync(direct, updatedContent, 'utf-8')
-      console.log(chalk.blue(direct))
+      // 替换并将内容写回文件
+      const newFileContent = fileContent.replace(/\/soyof\//g, '/')
+      fs.writeFileSync(direct, newFileContent, 'utf-8')
+      console.log(chalk.blue(direct), chalk.yellow(newFileContent.includes('/soyof/')))
     }
     return true
   }
