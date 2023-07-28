fc768808df82cf3e6563c05452a51197fdeb8a86
somuns <somuns.f13@gmail.com>
2023-07-28 16:12:39 +0800

    增加插件

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -146,14 +146,14 @@ module.exports = {
     }
     ],
     ['element-ui'],
-    [
-      'ribbon',
-      {
-        size: 90, // width of the ribbon, default: 90
-        opacity: 0.8, // opacity of the ribbon, default: 0.3
-        zIndex: -1 // z-index property of the background, default: -1
-      }
-    ],
+    // [
+    //   'ribbon',
+    //   {
+    //     size: 90, // width of the ribbon, default: 90
+    //     opacity: 0.8, // opacity of the ribbon, default: 0.3
+    //     zIndex: -1 // z-index property of the background, default: -1
+    //   }
+    // ],
     [
       'cursor-effects',
       {
@@ -197,6 +197,38 @@ module.exports = {
       'vuepress-plugin-image-viewer',
       {
         selector: '.theme-vdoing-content' // 你想要的插件起作用的页面的class或id
+      }
+    ],
+    [
+      "sakura", { // 樱花插件
+        num: 50,  // 默认数量
+        show: true, //  是否显示
+        zIndex: -1,   // 层级
+        img: {
+          replace: false,  // false 默认图 true 换图 需要填写httpUrl地址
+          httpUrl: ''     // 绝对路径
+        }
+      }
+    ],
+    [
+      "ribbon-animation", {
+        size: 90,   // 默认数据
+        opacity: 0.3,  //  透明度
+        zIndex: -1,   //  层级
+        opt: {
+          colorSaturation: "80%", // 色带HSL饱和度
+          colorBrightness: "60%", // 色带HSL亮度量
+          colorAlpha: 0.65, // 带状颜色不透明度
+          colorCycleSpeed: 6, // 在HSL颜色空间中循环显示颜色的速度有多快
+          verticalPosition: "center", // 从哪一侧开始Y轴 (top|min, middle|center, bottom|max, random)
+          horizontalSpeed: 200, // 到达屏幕另一侧的速度有多快
+          ribbonCount: 2, // 在任何给定时间，屏幕上会保留多少条带
+          strokeSize: 0, // 添加笔划以及色带填充颜色
+          parallaxAmount: -1, // 通过页面滚动上的因子垂直移动色带
+          animateSections: true // 随着时间的推移，为每个功能区添加动画效果
+        },
+        ribbonShow: false, //  点击彩带  true显示  false为不显示
+        ribbonAnimationShow: true  // 滑动彩带
       }
     ]
     // [
